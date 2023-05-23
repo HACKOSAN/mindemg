7415365eadb740abdbf820e8498400d9691f1644
HACKOSAN <mmuayyed9@gmail.com>
2023-05-24 01:55:10 +0300

    **massive fixing** --done by hacko -- :)
    -fixed header placement
    -fixed title placement/size
    -fixed old style look
    -added round borders
    -fixed (Our Features) placement
    -fixed feature section position
    -fixed server fetching stats
    -fixed footer uncompletion
    -fixed footer title
    -fixed icons placement
    -added pads
    -added animated colors

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const headers = new Headers();
-headers.append('Access-Control-Allow-Origin', '*');
 const serverAddressElement = document.getElementById('server-address');
 const serverStatusElement = document.getElementById('server-status');
 const playerCountElement = document.getElementById('player-count');
 
 function updateServerStatus() {
-  fetch('https://api.minetools.eu/ping/mc.classicc.tk')
+  fetch('/proxy-server-info')
     .then((response) => response.json())
     .then((data) => {
       serverAddressElement.textContent = data.hostname;
@@ -17,5 +15,12 @@ function updateServerStatus() {
       console.error(error);
     });
 }
-
 setInterval(updateServerStatus, 5000);
+
+// the fetch request URL is changed to '/proxy-server-info', 
+//which should be mapped to a server-side script on your web server. 
+//This server-side script will handle the request to the Minecraft server and return the necessary information. 
+//You'll need to implement the server-side script in a language of your choice (such as Node.js, PHP, Python, etc.) 
+//to fetch the Minecraft server info and respond with the required data.
+
+//Remember to set up the server-side script appropriately to handle the Minecraft server request and respond with the necessary data in JSON format.
